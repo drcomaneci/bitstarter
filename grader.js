6b37286edf691474d4e0c2516bd064bbd20a4cdd
Dragos Comaneci <dragos@comaneci.ro>
2013-08-15 11:39:59 +0000

    Updated index.html to new version from Coursera.

--- grader.js
+++ grader.js
@@ -24,8 +24,10 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var URL_DEFAULT = "https://raw.github.com/drcomaneci/bitstarter/master/index.html";
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -35,6 +37,10 @@ var assertFileExists = function(infile) {
     }
     return instr;
 };
+
+var assertDownloadFile = function(inurl) {
+   return inurl.toString();
+}
 
 var cheerioHtmlFile = function(htmlfile) {
     return cheerio.load(fs.readFileSync(htmlfile));
@@ -65,10 +71,23 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+	.option('-u, --url <url_link>', 'URL to html file', clone(assertDownloadFile), URL_DEFAULT)
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+    var file = program.file;
+    if (program.url != URL_DEFAULT){
+	rest.get(program.url).on('complete', function(result) {
+		if (result instanceof Error){
+			sys.puts("Error: "+result.message);
+			this.retry(5000);
+		} else {
+			fs.writeFileSync("tmp.html", result);
+			file="tmp.html";
+		}	
+    	});
+   }
+   var checkJson = checkHtmlFile(file, program.checks);
+   var outJson = JSON.stringify(checkJson, null, 4);
+   console.log(outJson);
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
